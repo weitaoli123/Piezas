65fc789fe3f536b268530b3c20a749c9dedb07af
weitaoli123 <47096222+weitaoli123@users.noreply.github.com>
2020-05-06 01:34:30 -0700

    Update PiezasTest.cpp

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -58,23 +58,23 @@ TEST(PiezasTest, gameStateTest1){
 	ASSERT_EQ(board.gameState(), Blank);
 }
 
-TEST(PiezasTest, gameStateTest){
+TEST(PiezasTest, gameStateTest2){
 	Piezas board;
 	
 	board.dropPiece(0);
-	board.dropPiece(3);
 	board.dropPiece(0);
-	board.dropPiece(3);
-	
+	board.dropPiece(1);
 	board.dropPiece(0);
-	board.dropPiece(2);
-	board.dropPiece(3);
-	board.dropPiece(2);
 	
 	board.dropPiece(2);
 	board.dropPiece(1);
+	board.dropPiece(3);
 	board.dropPiece(1);
-	board.dropPiece(1);
+	
+	board.dropPiece(2);
+	board.dropPiece(3);
+	board.dropPiece(2);
+	board.dropPiece(3);
 	
 	ASSERT_EQ(board.gameState(), X);
 }
